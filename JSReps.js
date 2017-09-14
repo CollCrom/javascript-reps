389ec267675d501663d0994b2b91afe40b70fb68
Collin Cromwell <collcrom@gmail.com>
2017-09-14 10:58:52 -0500

    finished round 3

--- JSReps.js
+++ JSReps.js
@@ -3,9 +3,18 @@ const lengths = (arr) => {
 	for(let i = 0; i < arr.length; i++)
 		lengthArr.push(arr[i].length);
 	return lengthArr;
-}
+};
 
 const transmogrifier = (num1, num2, num3) => {
 	return Math.pow((num1 * num2), num3);
-}
+};
+
+const toonify = (accent, sentence) => {
+	if(accent ==='daffy')
+		return sentence.split('s').join('th');
+	if(accent === 'elmer')
+		return sentence.split('r').join('w');
+	else
+		return sentence;
+};
 
